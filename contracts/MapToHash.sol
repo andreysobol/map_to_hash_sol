bcaf905a77bd92164fa5a34f33df0f631af65c02
Andrey Sobol <andreysobol@protonmail.com>
2022-08-16 17:24:40 +0200

    set

--- contracts/MapToHash.sol
+++ contracts/MapToHash.sol
@@ -5,8 +5,9 @@ pragma solidity >=0.7.0 <0.9.0;
 contract MatToHash {
 
     mapping(uint256 => uint256) public values;
+    bytes32 public result;
 
-    function getHash(uint256 size) public view returns (bytes32) {
+    function getHash(uint256 size) public returns (bytes32) {
         bytes memory tos = new bytes(32*size);
 
         for (uint256 i=0; i<size; i++) { 
@@ -16,8 +17,12 @@ contract MatToHash {
                 mstore(add(tos, pointer), v)
             }
         }
-        bytes32 result = sha256(tos);
+        result = sha256(tos);
         return result;
+    }
+
+    function set(uint256 key, uint256 value) public {
+        values[key] = value;
     }
 
 }
